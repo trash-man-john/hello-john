57183a2f44099bcd67676aa6f1e7dfd4731ca429
trash-man-john <83442344+trash-man-john@users.noreply.github.com>
2021-04-30 20:38:39 -0400

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -6,3 +6,7 @@ var ready = (callback) => {
 ready(() => {
     document.querySelector(".header").style.height = window.innerHeight + "px";
 })
+
+function basicPopup(url) {
+popupWindow = window.open(url,'popUpWindow','height=500,width=500,left=100,top=100,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes');
+	}
