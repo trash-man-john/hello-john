150643e6446874cf63f0ac28aea70bb3f3a80311
trash-man-john <83442344+trash-man-john@users.noreply.github.com>
2021-07-14 19:25:49 -0400

    Gay test revamp

--- docs/main.js
+++ docs/main.js
@@ -10,3 +10,23 @@ ready(() => {
 function basicPopup(url) {
 popupWindow = window.open(url,'popUpWindow','height=500,width=500,left=100,top=100,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes');
 	}
+
+  function pauseSong() {
+    var p = document.getElementById("pause");
+    p.style.display = "block";
+  }
+
+  function showCat() {
+    var c = document.getElementById("cat");
+    c.style.display = "block";
+  }
+
+  function hideSection() {
+    var g = document.getElementById("gay-section");
+    g.style.display = "none";
+  }
+
+  function showResults() {
+    var x = document.getElementById("gay-results");
+    x.style.display = "block";
+  }
