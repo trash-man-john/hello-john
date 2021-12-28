e56fb747a649c865003f52e4cbb8f7df0e7e87ea
trash-man-john <tmjisurmcm@gmail.com>
2021-12-28 11:45:37 -0500

    Completed Tabbed Store Page

--- docs/store.js
+++ docs/store.js
@@ -11,8 +11,40 @@ function basicPopup(url) {
   popupWindow = window.open(url, 'popUpWindow', 'height=500,width=500,left=100,top=100,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes');
 }
 
+function hidePlaceholder() {
+  var x = document.getElementById("page-default");
+  x.style.display = "none";
+}
+
+
 $(document).ready(function(){
   $(".nav-tabs a").click(function(){
     $(this).tab('show');
   });
+});
+
+
+$(function(){
+    $("#TabsClicked p").bind("click", function(){
+        $("#TabsClicked p").removeClass("clicked"); // Remove all highlights
+        $(this).addClass("clicked"); // Add the class only for actually clicked element
+    });
+});
+
+
+
+$('.carousel').carousel({
+  interval: false,
+});
+
+$('.carousel-inner').carousel({
+  interval: false,
+});
+
+$('.carousel-item').carousel({
+  interval: false,
+});
+
+$('.carousel-item.active').carousel({
+  interval: false,
 });
