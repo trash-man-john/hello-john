1717b54c2c04ebb15c4aaa522d5926fe79761ee9
trash-man-john <tmjisurmcm@gmail.com>
2022-08-30 18:02:02 -0400

    jump link nav

---
+++ docs/helpme.js
@@ -0,0 +1,6 @@
+// const navLinks = document.querySelectorAll('.nav-item')
+// const menuToggle = document.getElementById('main-navigation')
+// const bsCollapse = new bootstrap.Collapse(menuToggle)
+// navLinks.forEach((l) => {
+//     l.addEventListener('click', () => { bsCollapse.toggle() })
+// })
