d9eacda4e47a1a3be73be1df5c7a70b0385ccbda
trash-man-john <tmjisurmcm@gmail.com>
2022-08-30 19:27:53 -0400

    copy link

--- docs/helpme.js
+++ docs/helpme.js
@@ -4,3 +4,39 @@
 // navLinks.forEach((l) => {
 //     l.addEventListener('click', () => { bsCollapse.toggle() })
 // })
+
+const copyToClipboard = str => {
+  const el = document.createElement('textarea');
+  el.value = str;
+  el.setAttribute('readonly', '');
+  el.style.position = 'absolute';
+  el.style.left = '-9999px';
+  document.body.appendChild(el);
+  const selected =
+    document.getSelection().rangeCount > 0
+      ? document.getSelection().getRangeAt(0)
+      : false;
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+  if (selected) {
+    document.getSelection().removeAllRanges();
+    document.getSelection().addRange(selected);
+  }
+};
+
+// $( document ).ready(function() {
+//
+//
+// $('#thanks-button').click(function() {
+//     $('#thanks').css({
+//         'display': 'inline-block',
+//     });
+// });
+// });
+
+
+function balls() {
+    var thanks = document.getElementById("thank-you");
+    thanks.classList.toggle("thanks-show");
+  }
