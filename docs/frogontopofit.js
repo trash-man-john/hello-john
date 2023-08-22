1fa70ddf6cbc9b32bbe1b793e6db8261b02c849a
Aiden Fox Ivey <aiden@fox-ivey.com>
2023-08-22 00:08:37 -0400

    Fix frog countdown timeout

--- docs/frogontopofit.js
+++ docs/frogontopofit.js
@@ -15,6 +15,12 @@ var x = setInterval(function() {
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
+    // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("timer").innerHTML = "EXPIRED";
+  } else {
+
   // Display the result in the element with id="demo"
   document.getElementById("day-timer").innerHTML = days + " days";
 
@@ -23,12 +29,7 @@ var x = setInterval(function() {
   document.getElementById("minute-timer").innerHTML = minutes + " minutes";
 
   document.getElementById("second-timer").innerHTML = seconds + " seconds";
-
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("timer").innerHTML = "EXPIRED";
-  }
+}
 }, 1000);
 
 // function progress(timeleft, timetotal, $element) {
